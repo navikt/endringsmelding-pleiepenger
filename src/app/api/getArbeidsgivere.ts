cf6288da3b78400f10c4f6febac1766019744ca2
Frode Hansen <frode.hansen@bekk.no>
2021-10-22 17:04:12 +0200

    mock arbeidsgiver

--- src/app/api/getArbeidsgivere.ts
+++ src/app/api/getArbeidsgivere.ts
@@ -1,22 +1,29 @@
 import { failure, RemoteData, success } from '@devexperts/remote-data-ts';
 import { AxiosError } from 'axios';
-import { ApiEndpointPsb } from '../types/ApiEndpoint';
+// import { ApiEndpointPsb } from '../types/ApiEndpoint';
 import { Arbeidsgiver } from '../types/Arbeidsgiver';
-import api from './api';
+// import api from './api';
 
-type ArbeidsgiverRemoteType = {
-    organisasjoner: Arbeidsgiver[];
-};
+// type ArbeidsgiverRemoteType = {
+//     organisasjoner: Arbeidsgiver[];
+// };
 
 export type ArbeidsgiverRemoteData = RemoteData<AxiosError, Arbeidsgiver[]>;
 
 const getArbeidsgivereRemoteData = async (fom?: string, tom?: string): Promise<ArbeidsgiverRemoteData> => {
+    console.log(fom, tom);
+
     try {
-        const { data } = await api.psb.get<ArbeidsgiverRemoteType>(
-            ApiEndpointPsb.arbeidsgiver,
-            `fra_og_med=${fom}&til_og_med=${tom}`
+        // const { data } = await api.psb.get<ArbeidsgiverRemoteType>(
+        //     ApiEndpointPsb.arbeidsgiver,
+        //     `fra_og_med=${fom}&til_og_med=${tom}`
+        // );
+        return Promise.resolve(
+            success([
+                { navn: 'Johs broderi', organisasjonsnummer: '123451234' },
+                { navn: 'Noras sveiseri', organisasjonsnummer: '112233445' },
+            ])
         );
-        return Promise.resolve(success(data.organisasjoner));
     } catch (error) {
         return Promise.reject(failure(error));
     }
