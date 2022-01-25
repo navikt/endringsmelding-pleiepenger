14b98a69ffc7e5106176d580a7247341f6dfe214
Frode Hansen <frode.hansen@bekk.no>
2022-01-25 11:47:36 +0100

    devserver update

--- src/build/webpack/devserver.config.js
+++ src/build/webpack/devserver.config.js
@@ -3,8 +3,8 @@ const mustacheExpress = require('mustache-express');
 const envSettings = require('../../../envSettings');
 require('dotenv').config();
 
-const configureDevServer = (decoratorFragments, port) => ({
-    onBeforeSetupMiddleware: (devServer) => {
+const configureDevServer = (decoratorFragments) => ({
+    setupMiddlewares: (middlewares, devServer) => {
         devServer.app.engine('html', mustacheExpress());
         devServer.app.set('views', `${__dirname}/../../../dist/dev`);
         devServer.app.set('view engine', 'mustache');
@@ -22,8 +22,18 @@ const configureDevServer = (decoratorFragments, port) => ({
         devServer.app.get(/^\/(?!.*dist).*$/, (req, res) => {
             res.render('index.html', Object.assign(decoratorFragments));
         });
+        return middlewares;
     },
-    port,
+    devMiddleware: {
+        index: true,
+        stats: 'minimal',
+        publicPath: `${process.env.PUBLIC_PATH}/dist`,
+    },
+    static: {
+        directory: path.resolve(`${__dirname}/../../../dist`),
+        serveIndex: true,
+        watch: true,
+    },
 });
 
 module.exports = configureDevServer;
