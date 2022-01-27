d1eb639231ae709c7d9c2dc1a85a7fe1b10cbc58
Frode Hansen <frode.hansen@bekk.no>
2022-01-27 16:04:44 +0100

    Kvittering

---
+++ src/app/components/kvittering/Kvittering.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Box from '@navikt/sif-common-core/lib/components/box/Box';
+import CheckmarkIcon from '@navikt/sif-common-core/lib/components/checkmark-icon/CheckmarkIcon';
+import { Innholdstittel } from 'nav-frontend-typografi';
+import './kvittering.less';
+
+interface Props {
+    tittel: string;
+    children?: React.ReactNode;
+}
+
+const Kvittering = ({ tittel, children }: Props) => {
+    return (
+        <div className="kvittering">
+            <Box textAlignCenter={true} margin="none">
+                <CheckmarkIcon />
+                <Box margin="l">
+                    <Innholdstittel>{tittel}</Innholdstittel>
+                </Box>
+            </Box>
+            {children && <Box margin="xl">{children}</Box>}
+        </div>
+    );
+};
+
+export default Kvittering;
