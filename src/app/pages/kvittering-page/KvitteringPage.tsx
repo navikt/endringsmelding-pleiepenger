62c44b754c4f5c2d7eefc5388183259039ebb530
Frode Hansen <frode.hansen@bekk.no>
2022-02-01 10:31:58 +0100

    Lenke til Dine pleiepenger på kvittering

--- src/app/pages/kvittering-page/KvitteringPage.tsx
+++ src/app/pages/kvittering-page/KvitteringPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
 import { useLogSidevisning } from '@navikt/sif-common-amplitude';
+import Box from '@navikt/sif-common-core/lib/components/box/Box';
+import Knappelenke from '@navikt/sif-common-core/lib/components/knappelenke/Knappelenke';
 import Page from '@navikt/sif-common-core/lib/components/page/Page';
+import { getEnvironmentVariable } from '@navikt/sif-common-core/lib/utils/envUtils';
 import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
 import Kvittering from '../../components/kvittering/Kvittering';
 
@@ -14,6 +17,11 @@ const KvitteringPage: React.FunctionComponent = () => {
             <Kvittering tittel={<>Melding om endring er mottatt!</>}>
                 <p>Når vi har behandlet meldingen fra deg, får du svar fra oss på Ditt NAV.</p>
                 <p>Hvis du har registrert deg mot å motta digital post, får du svaret tilsendt i posten.</p>
+                <Box margin="xl">
+                    <Knappelenke type="hoved" href={getEnvironmentVariable('INNSYN_URL')}>
+                        Gå til dine pleiepenger
+                    </Knappelenke>
+                </Box>
             </Kvittering>
         </Page>
     );
